370ce5156583267327418a5a85c77799b2a0e021
yusuShin <tlsdbtn0507@naver.com>
2022-09-24 23:00:06 +0900

    DOCS: 풀 받을때 커밋안해서 오류난거 손보기

--- semi_project/src/main/java/test/com/activity/ActivityDAO.java
+++ semi_project/src/main/java/test/com/activity/ActivityDAO.java
@@ -15,15 +15,13 @@ public interface ActivityDAO {
 
 	// 액티비티 리스트
 	public List<ActivityVO> selectAll();
-//<<<<<<< HEAD
-//=======
-//	
-//	// 내 액티비티 전체 리스트
-//	public List<ActivityVO> mySelectAll(String member_name, String activityState);
-//	
-//	// 액티비티 추천 리스트      1
-////	public List<ActivityVO> selectAll();
-//>>>>>>> 0574ae638d7fa4a00e889ffeb0db092dac3d4166
+	
+	// 내 액티비티 전체 리스트
+	public List<ActivityVO> mySelectAll(String member_name, String activityState);
+	
+	// 액티비티 추천 리스트      1
+//	public List<ActivityVO> selectAll();
+
 
 	// 액티비티 검색하기
 	public List<ActivityVO> searchList(String key, String value);
