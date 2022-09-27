820c8a6b3328ad435796ad31dacb45c6db8f3cd3
hmhmchm <hyeonminchoi5@gmail.com>
2022-09-27 23:36:31 +0900

    feat : 모임 정보 수정, 라운드 모임 가입전 가입 후 상세페이지 , 모임 내 액티비티 리스트

--- semi_project/src/main/java/test/com/meeting/MeetingController.java
+++ semi_project/src/main/java/test/com/meeting/MeetingController.java
@@ -2,6 +2,8 @@ package test.com.meeting;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -24,7 +26,7 @@ import test.com.round.RoundVO;
 
 @WebServlet({ "/main_meeting_selectAll.do", "/main_meeting_searchList.do", "/main_meeting_insert.do",
 		"/main_meeting_insertOK.do", "/meeting_selectOne.do", "/mymeeting_list.do",
-		"/meeting_enter.do"})
+		"/meeting_enter.do","/meeting_update.do","/meeting_updateOK.do"})
 public class MeetingController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -37,6 +39,14 @@ public class MeetingController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setCharacterEncoding("UTF-8"); // UTF-8형식으로 바꿔주기 
+		
+		// test용--> 로그인구현 다 되면 지우기
+		HttpSession session = request.getSession(); // 객체 초기화
+		session.setMaxInactiveInterval(60);// interval 설정(초단위, 기본은 10~15분)
+		session.setAttribute("member_id", "2"); // -> 브라우저 X표 누르기전까지는 session에 저장됨.
+		// session에서 member_id를 가져옴.
+		String member_id = (String) session.getAttribute("member_id");
+		
 		String sPath = request.getServletPath();
 		
 		if (sPath.equals("/main_round_insert.do")) {
@@ -46,10 +56,12 @@ public class MeetingController extends HttpServlet {
 			String meeting_id = request.getParameter("meeting_id");
 
 			MeetingDAO dao = new MeetingDAOimpl();
-
-			MeetingVO vo = new MeetingVO();
+			
+			// 모임 유저 
+			MeetingUserVO vo = new MeetingUserVO();
 			vo.setMeeting_id(Long.parseLong(meeting_id));
-
+			vo.setMember_id(Long.parseLong(member_id));
+			
 			MeetingVO vo2 = dao.selectOne(vo);
 
 			request.setAttribute("vo2", vo2);
@@ -58,6 +70,7 @@ public class MeetingController extends HttpServlet {
 			response.getWriter().println(vo2.getName());
 			response.getWriter().println(vo2.getExplanation());
 			response.getWriter().println(vo2.getGender());
+			response.getWriter().println(vo2.getAge());
 			response.getWriter().println(vo2.getLocation());
 			response.getWriter().println(vo2.getSecret());
 			response.getWriter().println(vo2.getTotal_people());
@@ -82,12 +95,6 @@ public class MeetingController extends HttpServlet {
 			request.getRequestDispatcher("meeting/selectAll.jsp").forward(request, response);
 		} else if (sPath.equals("/mymeeting_list.do")) {
 
-			// test용--> 로그인구현 다 되면 지우기
-			HttpSession session = request.getSession(); // 객체 초기화
-			session.setMaxInactiveInterval(60);// interval 설정(초단위, 기본은 10~15분)
-			session.setAttribute("member_id", "1"); // -> 브라우저 X표 누르기전까지는 session에 저장됨.
-			// session에서 member_id를 가져옴.
-			String member_id = (String) session.getAttribute("member_id");
 
 			List<MeetingVO> vos = dao.mySelectAll(member_id);
 			// json으로 반환
@@ -104,15 +111,155 @@ public class MeetingController extends HttpServlet {
 		String dir_path = request.getServletContext().getRealPath("/upload");
 		System.out.println(dir_path);
 		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
 		String sPath = request.getServletPath();
 
+		long meeting_id = 0l;
+		
 		// session
 		HttpSession session = request.getSession();
 		session.setAttribute("member_id", "1");
 		String member_id = (String) session.getAttribute("member_id"); // member_name
 
 		if (sPath.equals("/main_meeting_insertOK.do")) {
+			// 현재 시간 구하기
+			SimpleDateFormat date_format = new SimpleDateFormat("yyyy-MM-dd");
+			Date now = new Date();         
+		
+			String name = "";
+			String explanation = "";
+			String gender = "";
+			String age = "";
+			String location = "";
+			String permission = "";
+			String secret = "";
+			int total_people = 0;
+			String image_url = "";
+			String creation_date = date_format.format(now);
 
+			int fileSizeMax = 1024 * 1024 * 100;
+			boolean isMultipartContent = ServletFileUpload.isMultipartContent(request);
+
+			if (isMultipartContent) {
+				DiskFileItemFactory factory = new DiskFileItemFactory();
+				factory.setSizeThreshold(fileSizeMax);
+				ServletFileUpload sfu = new ServletFileUpload(factory);
+				sfu.setFileSizeMax(fileSizeMax);// 파일 사이즈 제한
+
+				try {
+					List<FileItem> items = sfu.parseRequest(request);
+					for (FileItem item : items) {
+						if (item.isFormField()) { // name,id 받기
+							if (item.getFieldName().equals("name")) {
+								name = item.getString("UTF-8");
+							}
+							if (item.getFieldName().equals("explanation")) {
+								explanation = item.getString("UTF-8");
+							}
+							if (item.getFieldName().equals("gender")) {
+								gender = item.getString("UTF-8");
+							}
+							if (item.getFieldName().equals("age")) {
+								age = item.getString("UTF-8");
+							}
+							if (item.getFieldName().equals("location")) {
+								location = item.getString("UTF-8");
+							}
+							if (item.getFieldName().equals("permission")) {
+								permission = item.getString("UTF-8");
+							}
+							if (item.getFieldName().equals("secret")) {
+								secret = item.getString("UTF-8");
+							}
+							if (item.getFieldName().equals("total_people")) {
+								total_people = Integer.parseInt(item.getString("UTF-8"));
+							}
+							if (item.getFieldName().equals("creation_date")) {
+								creation_date = item.getString("UTF-8");
+							}
+						} else { // file정보받기.
+
+							image_url = FilenameUtils.getName(item.getName());
+//							image_url += "/"+id;
+							// 다른사람이 같은 이름으로 저장했을때, 덮어써질 수 있으므로 unique한 id나 system time 같이 저장.
+
+							File saveFile = new File(dir_path, image_url);
+
+							try {
+								item.write(saveFile);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+
+						} // end else
+
+					} // end for << items
+				} catch (FileUploadException e) {
+					e.printStackTrace();
+				}
+			}
+			MeetingDAO dao = new MeetingDAOimpl();
+			MeetingDAOimpl dao2 = new MeetingDAOimpl();
+			MeetingVO vo = new MeetingVO();
+			
+			meeting_id = dao2.meeting_id();
+			System.out.println("meeting_id 값 : " + meeting_id);
+			
+			vo.setMeeting_id(meeting_id);
+			vo.setName(name);
+			vo.setExplanation(explanation);
+			vo.setGender(gender);
+			vo.setAge(age);
+			vo.setLocation(location);
+			vo.setPermission(permission);
+			vo.setSecret(secret);
+			vo.setTotal_people(total_people);
+			vo.setImage_url(image_url);
+//			vo.setMember_id(1); 
+			vo.setMember_id(Long.parseLong(member_id));
+			vo.setCreation_date(creation_date);
+
+			int result = dao.insert(vo);
+
+			if (result == 1) {
+				System.out.println("모임이 개설되었습니다.");
+				
+				MeetingUserVO vo2 = new MeetingUserVO();
+				
+				vo2.setMeeting_id(meeting_id);
+				System.out.println("meeting_id 값  : " + meeting_id);
+				// 현재 로그인된 자신의 member_id로 넣어야함.
+				vo2.setMember_id(Long.parseLong(member_id));
+				vo2.setRole("MEETING_LEADER");
+				int result2 = dao.enter(vo2);
+
+				if (result2 == 1) {
+					System.out.println("모임장이 되었습니다.");
+				} else {
+					System.out.println("모임장이 될 수 없습니다.");
+				}
+				
+			} else {
+				System.out.println("모임개설을 실패하였습니다.");
+			}
+		}else if(sPath.equals("/meeting_enter.do")) {
+			
+			MeetingDAO dao22 = new MeetingDAOimpl();
+
+			MeetingUserVO vo = new MeetingUserVO();
+			vo.setMeeting_id(Long.parseLong(request.getParameter("meeting_id")));
+			// 현재 로그인된 자신의 member_id로 넣어야함.
+			vo.setMember_id(Long.parseLong(member_id));
+			vo.setRole("MEETING_MEMBER");
+			int result = dao22.enter(vo);
+
+			if (result == 1) {
+				System.out.println("모임에 입장하였습니다.");
+			} else {
+				System.out.println("모임에 입장 실패하였습니다.");
+			}
+		}else if(sPath.equals("/meeting_updateOK.do")) {
+			
 			String name = "";
 			String explanation = "";
 			String gender = "";
@@ -163,7 +310,7 @@ public class MeetingController extends HttpServlet {
 						} else { // file정보받기.
 
 							image_url = FilenameUtils.getName(item.getName());
-//							image_url += "/"+id;
+							image_url += "/"+member_id;
 							// 다른사람이 같은 이름으로 저장했을때, 덮어써질 수 있으므로 unique한 id나 system time 같이 저장.
 
 							File saveFile = new File(dir_path, image_url);
@@ -182,8 +329,9 @@ public class MeetingController extends HttpServlet {
 				}
 			}
 			MeetingDAO dao = new MeetingDAOimpl();
+			
 			MeetingVO vo = new MeetingVO();
-
+			vo.setMeeting_id(Long.parseLong(request.getParameter("meeting_id")));
 			vo.setName(name);
 			vo.setExplanation(explanation);
 			vo.setGender(gender);
@@ -193,38 +341,14 @@ public class MeetingController extends HttpServlet {
 			vo.setSecret(secret);
 			vo.setTotal_people(total_people);
 			vo.setImage_url(image_url);
-//			vo.setMember_id(1); 
-			vo.setMember_id(Long.parseLong(member_id));
-
-			int result = dao.insert(vo);
-
+			
+			System.out.println(name + " " + total_people);
+			
+			int result = dao.update(vo);
 			if (result == 1) {
-				System.out.println("모임이 개설되었습니다.");
-			} else {
-				System.out.println("모임개설을 실패하였습니다.");
-			}
-		}else if(sPath.equals("/meeting_enter.do")) {
-			
-//			HttpSession session = request.getSession(); //객체 초기화
-//			session.setMaxInactiveInterval(60);//interval 설정(초단위, 기본은 10~15분)
-//			session.setAttribute("member_id", "1"); //-> 브라우저 X표 누르기전까지는 session에 저장됨.
-			
-			//session에서 member_id를 가져옴.
-//			String member_id = (String) session.getAttribute("member_id");
-			
-			MeetingDAO dao = new MeetingDAOimpl();
-
-			MeetingUserVO vo = new MeetingUserVO();
-			vo.setMeeting_id(Long.parseLong(request.getParameter("meeting_id")));
-			// 현재 로그인된 자신의 member_id로 넣어야함.
-			vo.setMember_id(Long.parseLong(member_id));
-			vo.setRole("MEETING_MEMBER");
-			int result = dao.enter(vo);
-
-			if (result == 1) {
-				System.out.println("모임에 입장하였습니다.");
-			} else {
-				System.out.println("모임에 입장 실패하였습니다.");
+				System.out.println("모임정보수정이 완료.");
+			}else {
+				System.out.println("모임정보수정이 실패.");
 			}
 		}
 	}
